39caa897ba2097d952ed9ca7942f07f4d5a4845c
isra9710 <isra.rios.con@gmail.com>
2022-11-02 20:56:43 -0600

    Doceavo commit

--- Main.java
+++ Main.java
@@ -5,5 +5,6 @@ class Main{
 		System.out.println("La conexión con Github se ha hecho");
 		System.out.println("Un cambio más");
 		System.out.println("Cuarto cambio");
+		System.out.println("Cambio en la rama development")
 	}
 }
